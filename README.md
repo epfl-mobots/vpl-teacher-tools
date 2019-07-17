a3aaaf19abbb79b73c9fd1d7b46002821e9877f5
Yves Piguet <yves.piguet@epfl.ch>
2019-07-17 14:59:59 +0200

    Basic pairing web app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vpl-teacher-tools
+
+Basic prototypes of VPL teacher tools to test the server
---
+++ ns.js
@@ -0,0 +1,20 @@
+/*
+	Copyright 2018-2019 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE,
+	Miniature Mobile Robots group, Switzerland
+	Author: Yves Piguet
+
+	Licensed under the 3-Clause BSD License;
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	https://opensource.org/licenses/BSD-3-Clause
+*/
+
+/** @fileoverview
+
+Base namespace for everything related to the JavaScript implementation
+of the VPL teacher tools.
+
+*/
+
+/**	Namespace */
+var VPLTeacherTools = {};
