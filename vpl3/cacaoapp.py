ea2dbf91c9b9307f3f87c3f7d9eb6ff85051ff6a
Yves Piguet <yves.piguet@epfl.ch>
2019-10-23 14:31:16 +0200

    Non-VPL pyobjc application shell moved to separate file

---
+++ vpl3/cacaoapp.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+# Author: Yves Piguet, EPFL
+# Please don't redistribute without author's permission.
+
+# pyobjc-based application class (for macOS only)
+
+# macOS: sudo python3 -m pip install pyobjc
+from Cocoa import *
+from Foundation import NSObject
+from objc import super
+
+class ApplicationObjCShell(NSApplication):
+
+    def init(self):
+        self = super(NSApplication, self).init()
+        if self is None:
+            return self
+        self.actionArray = []
+        self.menuNames = []
+        self.menuItems = []
+        return self
+
+    def setAction_forControl_(self, action, control):
+        control.setTarget_(self)
+        control.setAction_("forwardAction:")
+        control.setTag_(len(self.actionArray))
+        self.actionArray.append(action)
+
+    def addMenu_withItems_(self, menuName, menuItems):
+        self.menuNames.append(menuName)
+        self.menuItems.append(menuItems)
+
+    def start(self):
+        NSApplication.sharedApplication()
+        NSApp.setActivationPolicy_(NSApplicationActivationPolicyRegular)
+        menubar = NSMenu.new().autorelease()
+
+        def add_menu(name, items):
+            menu_hook = NSMenuItem.new().autorelease()
+            if name is not None:
+                menu = NSMenu.alloc().initWithTitle_(name).autorelease()
+            else:
+                menu = NSMenu.new().autorelease()
+            menu.setAutoenablesItems_(False)
+            menubar.addItem_(menu_hook)
+            sel = objc.selector(self.forwardAction_, signature=b"v@:@")
+            for i in range(len(items)):
+                menu_item = (NSMenuItem
+                             .alloc()
+                             .initWithTitle_action_keyEquivalent_(items[i][0],
+                                                                  sel,
+                                                                  items[i][1])
+                             .autorelease())
+                menu_item.setTarget_(self)
+                menu_item.setTag_(len(self.actionArray))
+                self.actionArray.append(items[i][2])
+                menu_item.setEnabled_(True)
+                menu.addItem_(menu_item)
+            menu_hook.setSubmenu_(menu)
+
+        add_menu(None, [
+            (
+                "Quit " + str(NSProcessInfo.processInfo().processName()),
+                "q",
+                NSApp.terminate_
+            ),
+        ])
+        for i in range(len(self.menuNames)):
+            add_menu(self.menuNames[i], self.menuItems[i])
+        NSApp.setMainMenu_(menubar)
+
+    def run(self):
+        NSApp.activateIgnoringOtherApps_(True)
+        NSApp.run()
+
+    def createWindowWithTitle_width_height_x_y_(self, title, width, height, x, y):
+        window = NSWindow.alloc().initWithContentRect_styleMask_backing_defer_(NSMakeRect(0, 0, width, height), 1, 2, False).autorelease()
+        window.cascadeTopLeftFromPoint_(NSMakePoint(x, y))
+        window.setTitle_(title)
+        window.makeKeyAndOrderFront_(0)
+        return window
+
+    def addLabelToWindow_title_width_x_y_(self, window, title, width, x, y):
+        label = (NSTextField.alloc()
+                  .initWithFrame_(NSMakeRect(x, y, width, 25))
+                  .autorelease())
+        label.setFont_(NSFont.systemFontOfSize_(14))
+        label.setDrawsBackground_(False)
+        label.setBordered_(False)
+        label.setEditable_(False)
+        label.setSelectable_(True)
+        label.setStringValue_(title)
+        window.contentView().addSubview_(label)
+        return label
+
+    def addButtonToWindow_title_action_width_x_y_(self, window, title, action, width, x, y):
+        button = (NSButton.alloc()
+                  .initWithFrame_(NSMakeRect(x, y, width, 25))
+                  .autorelease())
+        button.setBezelStyle_(NSRoundedBezelStyle)
+        button.setTitle_(title)
+        self.setAction_forControl_(action, button)
+        window.contentView().addSubview_(button)
+        return button
+
+    @objc.IBAction
+    def forwardAction_(self, sender):
+        tag = sender.tag()
+        self.actionArray[tag](sender)
--- vpl3/objcapp.py
+++ vpl3/objcapp.py
@@ -6,110 +6,7 @@
 # server gui with objc (for macOS only)
 
 from vpl3.baseapp import ApplicationBase
-
-# macOS: sudo python3 -m pip install pyobjc
-from Cocoa import *
-from Foundation import NSObject
-from objc import super
-
-class ApplicationObjCShell(NSApplication):
-
-    def init(self):
-        self = super(NSApplication, self).init()
-        if self is None:
-            return self
-        self.actionArray = []
-        self.menuNames = []
-        self.menuItems = []
-        return self
-
-    def setAction_forControl_(self, action, control):
-        control.setTarget_(self)
-        control.setAction_("forwardAction:")
-        control.setTag_(len(self.actionArray))
-        self.actionArray.append(action)
-
-    def addMenu_withItems_(self, menuName, menuItems):
-        self.menuNames.append(menuName)
-        self.menuItems.append(menuItems)
-
-    def start(self):
-        NSApplication.sharedApplication()
-        NSApp.setActivationPolicy_(NSApplicationActivationPolicyRegular)
-        menubar = NSMenu.new().autorelease()
-
-        def add_menu(name, items):
-            menu_hook = NSMenuItem.new().autorelease()
-            if name is not None:
-                menu = NSMenu.alloc().initWithTitle_(name).autorelease()
-            else:
-                menu = NSMenu.new().autorelease()
-            menu.setAutoenablesItems_(False)
-            menubar.addItem_(menu_hook)
-            sel = objc.selector(self.forwardAction_, signature=b"v@:@")
-            for i in range(len(items)):
-                menu_item = (NSMenuItem
-                             .alloc()
-                             .initWithTitle_action_keyEquivalent_(items[i][0],
-                                                                  sel,
-                                                                  items[i][1])
-                             .autorelease())
-                menu_item.setTarget_(self)
-                menu_item.setTag_(len(self.actionArray))
-                self.actionArray.append(items[i][2])
-                menu_item.setEnabled_(True)
-                menu.addItem_(menu_item)
-            menu_hook.setSubmenu_(menu)
-
-        add_menu(None, [
-            (
-                "Quit " + str(NSProcessInfo.processInfo().processName()),
-                "q",
-                NSApp.terminate_
-            ),
-        ])
-        for i in range(len(self.menuNames)):
-            add_menu(self.menuNames[i], self.menuItems[i])
-        NSApp.setMainMenu_(menubar)
-
-    def run(self):
-        NSApp.activateIgnoringOtherApps_(True)
-        NSApp.run()
-
-    def createWindowWithTitle_width_height_x_y_(self, title, width, height, x, y):
-        window = NSWindow.alloc().initWithContentRect_styleMask_backing_defer_(NSMakeRect(0, 0, width, height), 1, 2, False).autorelease()
-        window.cascadeTopLeftFromPoint_(NSMakePoint(x, y))
-        window.setTitle_(title)
-        window.makeKeyAndOrderFront_(0)
-        return window
-
-    def addLabelToWindow_title_width_x_y_(self, window, title, width, x, y):
-        label = (NSTextField.alloc()
-                  .initWithFrame_(NSMakeRect(x, y, width, 25))
-                  .autorelease())
-        label.setFont_(NSFont.systemFontOfSize_(14))
-        label.setDrawsBackground_(False)
-        label.setBordered_(False)
-        label.setEditable_(False)
-        label.setSelectable_(True)
-        label.setStringValue_(title)
-        window.contentView().addSubview_(label)
-        return label
-
-    def addButtonToWindow_title_action_width_x_y_(self, window, title, action, width, x, y):
-        button = (NSButton.alloc()
-                  .initWithFrame_(NSMakeRect(x, y, width, 25))
-                  .autorelease())
-        button.setBezelStyle_(NSRoundedBezelStyle)
-        button.setTitle_(title)
-        self.setAction_forControl_(action, button)
-        window.contentView().addSubview_(button)
-        return button
-
-    @objc.IBAction
-    def forwardAction_(self, sender):
-        tag = sender.tag()
-        self.actionArray[tag](sender)
+from vpl3.cacaoapp import ApplicationObjCShell
 
 
 class Application(ApplicationBase):
@@ -120,7 +17,7 @@ class Application(ApplicationBase):
         self.app_objc = ApplicationObjCShell.alloc().init()
         self.app_objc.addMenu_withItems_("File", [
             (
-                "Open in Browser",
+                "Open Tools in Browser",
                 "b",
                 lambda sender: self.start_browser()
             )
@@ -128,16 +25,16 @@ class Application(ApplicationBase):
         self.app_objc.start()
         window = self.app_objc.createWindowWithTitle_width_height_x_y_(
             "VPL Server - " + self.address,
-            300, 200,
+            300, 100,
             20, 20
         )
         self.status = self.app_objc.addLabelToWindow_title_width_x_y_(window,
                                                                       "",
-                                                                      250, 10, 160)
+                                                                      250, 10, 60)
         self.app_objc.addButtonToWindow_title_action_width_x_y_(window,
-                                                                "Open in Browser",
+                                                                "Open tools in browser",
                                                                 lambda sender: self.start_browser(),
-                                                                180, 10, 130)
+                                                                180, 60, 20)
 
     def main_loop(self):
         self.app_objc.run()
