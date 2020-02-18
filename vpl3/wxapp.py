dd48d853e25dd6a43cf695d5826abb3e6732f5ed
Yves Piguet <yves.piguet@epfl.ch>
2020-02-18 14:46:07 +0100

    Window of wx app made similar to other versions

--- vpl3/wxapp.py
+++ vpl3/wxapp.py
@@ -6,9 +6,65 @@
 # server gui with wxPython
 
 from vpl3.baseapp import ApplicationBase
+from vpl3.urlutil import URLUtil
 
-import wx  # Ubuntu: sudo apt install python3-wxgtk4.0
+import wx
+# Ubuntu: sudo apt install python3-wxgtk4.0
+# macOS: download last release from https://wxpython.org/ then
+# python3 -m wheel unpack ~/Downloads/...; sudo python3 -m pip install wxPython
 
+class QRControl(wx.Control):
+    # see implementation of GenStaticText in wx/lib/stattext.py
+
+    def __init__(self, parent, ID=-1,
+                 pos=wx.DefaultPosition, size=wx.DefaultSize,
+                 style=0,
+                 name="QRCode",
+                 text=""):
+        wx.Control.__init__(self, parent, ID, pos, size, style | wx.NO_BORDER,
+                            wx.DefaultValidator, name)
+        self.InheritAttributes()
+        self.SetInitialSize(size)
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_ERASE_BACKGROUND, lambda event: self.OnEraseBackground())
+
+        self.size = size
+        self.text = text
+
+    def SetText(self, text):
+        self.text = text
+        self.Refresh()
+
+    def DoGetBestSize(self):
+        return wx.Size(self.size[0], self.size[1])
+
+    def AcceptsFocus(self):
+        return False
+
+    def OnPaint(self, event):
+        dc = wx.BufferedPaintDC(self)
+
+        width, height = self.GetClientSize()
+        backColor = self.GetBackgroundColour()
+        dc.SetBackground(wx.Brush(backColor, wx.SOLID))
+        dc.Clear()
+
+        import qrcode
+        qr = qrcode.QRCode()
+        qr.add_data(self.text)
+        qr.make()
+        n = qr.modules_count
+        s = width // (n + 8)  # margin = at least 4 modules
+        margin = (width - n * s) / 2
+        dc.SetBrush(wx.Brush(wx.BLACK, wx.SOLID))
+        for i in range(n):
+            for j in range(n):
+                if qr.modules[i][j]:
+                    dc.DrawRectangle(margin + s * j, margin + s * i, s, s)
+
+    def OnEraseBackground(self):
+        pass
 
 class Application(ApplicationBase, wx.App):
 
@@ -16,23 +72,34 @@ class Application(ApplicationBase, wx.App):
         ApplicationBase.__init__(self, **kwargs)
         wx.App.__init__(self)
 
+        size = wx.Size(600, 280)
         self.frame = wx.Frame(None,
                               title="VPL Server - " + self.tt_url(True),
-                              size=wx.Size(800, 500))
+                              size=size,
+                              style=wx.DEFAULT_FRAME_STYLE & ~wx.RESIZE_BORDER)
+        self.frame.SetMaxSize(size)
+        self.frame.SetMinSize(size)
         self.frame.Bind(wx.EVT_CLOSE,
                         lambda event: self.quit())
-        self.frame.Show()
 
-        panel = wx.Panel(self.frame)
-        vbox = wx.BoxSizer(wx.VERTICAL)
+        self.panel = wx.Panel(self.frame)
+        self.vbox = wx.BoxSizer(wx.VERTICAL)
 
-        self.ws_info_label = wx.StaticText(panel, -1)
-        vbox.Add(self.ws_info_label, 0, wx.ALL | wx.EXPAND, 5)
+        self.status = wx.StaticText(self.panel, -1)
+        self.vbox.Add(self.status, 0, wx.ALL | wx.EXPAND, 10)
 
-        self.log = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_READONLY)
-        vbox.Add(self.log, 1, wx.ALL | wx.EXPAND, 5)
+        self.robot_status = wx.StaticText(self.panel, -1)
+        self.vbox.Add(self.robot_status, 0, wx.ALL | wx.EXPAND, 10)
 
-        panel.SetSizer(vbox)
+        launch_button = wx.Button(self.panel, -1, "Teacher Tools")
+        launch_button.Bind(wx.EVT_BUTTON, lambda event: self.start_browser_tt())
+        self.vbox.Add(launch_button, 0, wx.CENTER, 10)
+
+        self.qr_code = QRControl(self.panel, size=(160, 160), text="?")
+        self.vbox.Add(self.qr_code, 0, wx.CENTER, 10)
+
+        self.vbox.SetSizeHints(self.panel)
+        self.panel.SetSizer(self.vbox)
 
         self.update_connection()
 
@@ -52,15 +119,24 @@ class Application(ApplicationBase, wx.App):
                         lambda event: self.quit(),
                         exit_item)
 
+        self.update_qr_code()
+
+        self.frame.Show()
+
     def main_loop(self):
         self.MainLoop()
 
-    def writeln(self, str):
-        self.log.AppendText(str + "\n")
-        self.log.PageDown()
+    def update_qr_code(self):
+        path = self.tt_abs_path()
+        url = URLUtil.teacher_tools_URL(port=self.http_port,
+                                        path=path)
+        self.qr_code.SetText(url)
 
     def show_connection_status(self, str):
-        self.ws_info_label.SetLabel(str)
+        self.status.SetLabel(str)
+
+    def show_robot_status(self, str):
+        self.robot_status.SetLabel(str)
 
     def exit_app(self):
         self.frame.Destroy()
