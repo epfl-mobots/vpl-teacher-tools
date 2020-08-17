c4f52041c3cc08eb19b03bbef6700994e87f73df
Yves Piguet <yves.piguet@epfl.ch>
2020-08-17 17:23:22 +0200

    More robust launcher for windows
    Former-commit-id: 3c51c13f11768ea531e5aeb8e8c862b6caa2a1f2

--- launch_windows.bat
+++ launch_windows.bat
@@ -1,5 +1,24 @@
 @echo off
-python3 -m pip install websocket websockets qrcode
-start pythonw3 launch_tk.py
+set p=python
+set pw=pythonw
+where /q "%p%"
+if errorlevel 1 (
+	set p=python3
+	set pw=pythonw3
+	where /q "%p%"
+	if errorlevel 1 (
+		set p="%userprofile%\AppData\Local\Programs\Python\Launcher\py.exe"
+		set pw="%userprofile%\AppData\Local\Programs\Python\Launcher\pyw.exe"
+		if not exist "%p%" (
+			set p="%userprofile%\AppData\Local\Microsoft\WindowsApps\python3.exe"
+			set pw="%userprofile%\AppData\Local\Microsoft\WindowsApps\pythonw3.exe"
+			if not exist "%p" (
+				set p=python
+				set pw=pythonw
+			)
+		)
+	)
+)
+"%p%" -m pip install websocket websockets qrcode
+start "%pw%" launch_tk.py
 exit
-
