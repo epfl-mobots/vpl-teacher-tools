58a26bde8c322b65cb9866eddc1d3f97a565c5c7
Yves Piguet <yves.piguet@epfl.ch>
2020-10-22 17:36:02 +0200

    Setup file to create an msi package for Windows with cx_Freeze

---
+++ setup_exe.py
@@ -0,0 +1,29 @@
+# Setup file to create Windows app using cs_Freeze
+# To run it: in PowerShell, type
+#   python3 setup_exe.py bdist_msi
+# The result is a .msi file in directory dist
+
+import sys, os
+from cx_Freeze import setup, Executable
+
+__version__ = "0.0.1"
+
+include_files = []
+excludes = []
+packages = ["os", "json",]
+
+setup(
+		name="vpl3tt",
+		description="VPL3 Teacher Tools",
+		version=__version__,
+		options={
+			"build_exe": {
+				"packages": packages,
+				"include_files": include_files,
+				"excludes": excludes,
+				"include_msvcr": True,
+			},
+		},
+		executables=[Executable("launch_tk.py", base="Win32GUI")],
+)
+
