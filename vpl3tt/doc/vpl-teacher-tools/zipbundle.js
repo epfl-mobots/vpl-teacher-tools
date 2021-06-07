e0d8f478eef41772304084931f5257d146f33603
Yves Piguet <yves.piguet@epfl.ch>
2021-06-07 11:36:16 +0200

    Zip bundle manifest parsing

--- vpl3tt/doc/vpl-teacher-tools/zipbundle.js
+++ vpl3tt/doc/vpl-teacher-tools/zipbundle.js
@@ -69,12 +69,14 @@ VPLTeacherTools.ZipBundle = function () {
 	this.zip = null;
 	this.toc = [];
 	this.pathPrefix = "";
+	this.manifest = new VPLTeacherTools.ZipBundle.Manifest();
 };
 
 VPLTeacherTools.ZipBundle.prototype.load = function (zipContent, cb) {
 	this.zip = new VPLTeacherTools.JSZip();
 	this.toc = [];
 	this.pathPrefix = "";
+	var self = this;
 	this.zip.loadAsync(zipContent)
 		.then(() => {
 
@@ -85,10 +87,96 @@ VPLTeacherTools.ZipBundle.prototype.load = function (zipContent, cb) {
 				this.pathPrefix = rootEntries[0].name;
 			}
 
-			this.toc = this.zip.getCompleteFileList();
+			this.toc = this.zip.getCompleteFileList()
+				.map((path) => path.slice(this.pathPrefix.length));
 
-			if (cb) {
+			var manifestFile = self.zip.file(this.pathPrefix + "manifest.txt");
+			if (manifestFile) {
+				manifestFile.async("string").then((manifestSrc) => {
+					self.manifest.parse(manifestSrc, self.toc);
+					if (cb) {
+						cb(this);
+					}
+				});
+			} else if (cb) {
 				cb(this);
 			}
 		});
+};
+
+/**
+	@constructor
+*/
+VPLTeacherTools.ZipBundle.Manifest = function () {
+	this.src = "";
+	/** @type {Array.<VPLTeacherTools.ZipBundle.Manifest.File>} */
+	this.files = [];
+};
+
+/**
+	@param {string} src
+	@param {Array.<string>} filenames
+	@return {void}
+*/
+VPLTeacherTools.ZipBundle.Manifest.prototype.parse = function (src, filenames) {
+	this.files = [];
+	var lines = src.split("\n").map((line) => line.trim());
+	for (var i = 0; i < lines.length; i++) {
+		if (lines[i].slice(-1) === ":") {
+			var type = VPLTeacherTools.ZipBundle.Manifest.File.Type.unknown;
+			switch (lines[i].slice(0, -1).trim().toLowerCase()) {
+			case "vpl3":
+				type = VPLTeacherTools.ZipBundle.Manifest.File.Type.vpl3;
+				break;
+			case "ui":
+				type = VPLTeacherTools.ZipBundle.Manifest.File.Type.ui;
+				break;
+			case "attention":
+				type = VPLTeacherTools.ZipBundle.Manifest.File.Type.attention;
+				break;
+			case "doc":
+				type = VPLTeacherTools.ZipBundle.Manifest.File.Type.doc;
+				break;
+			case "statement":
+				type = VPLTeacherTools.ZipBundle.Manifest.File.Type.statement;
+				break;
+			}
+			if (type !== VPLTeacherTools.ZipBundle.Manifest.File.Type.unknown) {
+				var re = /([-_\/.a-z0-9]+)(\s+\([^)]*\))?$/i;
+				for (; i + 1 < lines.length; i++) {
+					var line = lines[i + 1];
+					if (line != "") {
+						var r = re.exec(line);
+						if (r == null) {
+							break;
+						}
+						var filename = r[1];
+						if (filenames.indexOf(filename) >= 0) {
+							this.files.push(new VPLTeacherTools.ZipBundle.Manifest.File(filename, type));
+						}
+					}
+				}
+			}
+		}
+	}
+};
+
+/**
+	@constructor
+	@param {string} filename
+	@param {VPLTeacherTools.ZipBundle.Manifest.File.Type} type
+*/
+VPLTeacherTools.ZipBundle.Manifest.File = function (filename, type) {
+	this.filename = filename;
+	this.type = type;
+};
+
+/** @enum {number} */
+VPLTeacherTools.ZipBundle.Manifest.File.Type = {
+	unknown: 0,
+	vpl3: 1,
+	ui: 2,
+	attention: 3,
+	doc: 4,
+	statement: 5
 };
