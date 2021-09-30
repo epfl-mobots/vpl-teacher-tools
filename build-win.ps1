95d83a3585f1091b75ae4a29c15dd26a0db1e918
Michael Bonani <michael.bonani@mobsya.org>
2021-09-30 11:42:45 +0200

    update builds (#1)
    * update builds

--- build-win.ps1
+++ build-win.ps1
@@ -4,7 +4,7 @@
 python3 setup_cx_freeze.py bdist_msi
 
 $compress = @{
-    Path = ".\dist\VPL3Server-cx-Freeze-0.1.0-amd64.msi", "readme-win.txt"
+    Path = ".\dist\VPL3Server-cx-Freeze-0.1.0-win64.msi", "readme-win.txt"
     CompressionLevel = "Fastest"
     DestinationPath = "VPL3Server-win.zip"
 }
