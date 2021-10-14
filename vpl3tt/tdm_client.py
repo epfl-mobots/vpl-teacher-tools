004e5007d0bdcf1b832bbd135ba563284b509b30
Yves Piguet <yves.piguet@epfl.ch>
2021-10-14 11:16:06 +0200

    Check for TDM with tcp instead of websocket

--- vpl3tt/tdm_client.py
+++ vpl3tt/tdm_client.py
@@ -4,7 +4,6 @@
 
 # Communication with TDM (just checking it's there)
 
-import websocket
 
 
 class TDMClient:
@@ -12,7 +11,27 @@ class TDMClient:
     def __init__(self, ws=None):
         self.ws = ws or "ws://127.0.0.1:8597"
 
+        import re
+        r = re.match("ws://(.*):([\d]+)(/.*)?$", self.ws)
+        if r is None:
+            # invalid self.ws
+            raise Exception(f"invalid websocket address {self.ws}")
+        self.host = r.group(1)
+        self.port = int(r.group(2))
+
     def check_connection(self):
+        import socket
+
+        try:
+            socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.connect((self.host, self.port))
+            socket.close()
+        except Exception as e:
+            return False
+        return True
+
+    def check_connection_with_ws(self):
+        import websocket
         try:
             ws = websocket.create_connection(self.ws)
             ws.close()
