b3d9d222791a27282479c0270845f1e6ba5be2a8
Yves Piguet <yves.piguet@epfl.ch>
2021-10-26 11:28:40 +0200

    In setup tab, panels not refreshed unless needed

--- vpl3tt/doc/vpl-teacher-tools/pairing-gui.js
+++ vpl3tt/doc/vpl-teacher-tools/pairing-gui.js
@@ -107,10 +107,6 @@ function fillRobotTable(robotArray, pairing) {
 }
 
 function fillStudentTable(studentArray, pairing) {
-	clearTable("students",
-		[VPLTeacherTools.translateArray(["Name", "Class"])],
-		["", "narrow"]);
-
 	var table = document.getElementById("students");
 
 	function addRow(studentName, studentClass, groupId) {
@@ -152,16 +148,22 @@ function fillStudentTable(studentArray, pairing) {
 		table.appendChild(tr);
 	}
 
-    if (studentArray.length > 0) {
-        studentArray.forEach(function (student) {
-            addRow(student.name, student["class"], student.group_id);
-        });
-        document.getElementById("student-help").style.display = "block";
-        document.getElementById("nostudent-help").style.display = "none";
-    } else {
-        document.getElementById("student-help").style.display = "none";
-        document.getElementById("nostudent-help").style.display = "block";
-    }
+	if (!pairing.noRedraw) {
+		clearTable("students",
+			[VPLTeacherTools.translateArray(["Name", "Class"])],
+			["", "narrow"]);
+
+		if (studentArray.length > 0) {
+			studentArray.forEach(function (student) {
+				addRow(student.name, student["class"], student.group_id);
+			});
+			document.getElementById("student-help").style.display = "block";
+			document.getElementById("nostudent-help").style.display = "none";
+		} else {
+			document.getElementById("student-help").style.display = "none";
+			document.getElementById("nostudent-help").style.display = "block";
+		}
+	}
 }
 
 function qrCodeSize() {
@@ -188,213 +190,216 @@ function fillGroupTable(groupArray, pairing) {
 		return true;
 	}
 
-	clearTable("groups");
-	var table = document.getElementById("groups");
+	if (!pairing.noRedraw) {
 
-	groupArray.forEach(function (group) {
-		function select() {
-			pairing.selectGroup(group.group_id);
-			fillGroupTable(groupArray, pairing);
-		}
+		clearTable("groups");
+		var table = document.getElementById("groups");
 
-		if (group.students && group.students.length > 0) {
-			var tr = document.createElement("tr");
+		groupArray.forEach(function (group) {
+			function select() {
+				pairing.selectGroup(group.group_id);
+				fillGroupTable(groupArray, pairing);
+			}
 
-			var td = document.createElement("td");
-			td.className = pairing.isGroupSelected(group.group_id) ? "rect container selected" : "rect container";
-			group.students.forEach(function (studentName, i) {
-				var span = document.createElement("span");
-				span.className = "rect";
-				span.textContent = studentName;
+			if (group.students && group.students.length > 0) {
+				var tr = document.createElement("tr");
 
-				// drag student name
-				span.draggable = true;
-				span.addEventListener("dragstart", function (ev) {
-					ev.dataTransfer.setData("text/plain", "G/" + studentName);
-					ev.dataTransfer.effectAllowed = "move";
+				var td = document.createElement("td");
+				td.className = pairing.isGroupSelected(group.group_id) ? "rect container selected" : "rect container";
+				group.students.forEach(function (studentName, i) {
+					var span = document.createElement("span");
+					span.className = "rect";
+					span.textContent = studentName;
+
+					// drag student name
+					span.draggable = true;
+					span.addEventListener("dragstart", function (ev) {
+						ev.dataTransfer.setData("text/plain", "G/" + studentName);
+						ev.dataTransfer.effectAllowed = "move";
+					});
+
+					td.appendChild(span);
 				});
-
-				td.appendChild(span);
-			});
-			td.addEventListener("click", select);
-
-			// drop student or robot name
-			tr.addEventListener("dragenter", checkDragType);
-			tr.addEventListener("dragover", checkDragType);
-			tr.addEventListener("drop", function (ev) {
-				ev.stopPropagation();
-				ev.preventDefault();
-				var data = ev.dataTransfer.getData("text/plain");
-				if (data && /^[SG]\//.test(data)) {
-					ev.stopPropagation();
-					var studentName = data.slice(2);
-					pairing.addStudentToGroup(studentName, group.group_id, true);
-				} else if (data && /^R\//.test(data)) {
-					ev.stopPropagation();
-					dataParts = data.split("/");
-					var robotName = dataParts[1];
-					var robotNiceName = dataParts[2];
-					pairing.beginSession(robotName, group.group_id);
-				}
-			});
-
-			tr.appendChild(td);
-
-			// robot
-			td = document.createElement("td");
-			tr.appendChild(td);
-			if (group.pair) {
-				td.className = pairing.isGroupSelected(group.group_id) ? "rect selected" : "rect";
-				td.textContent = (/^\{/.test(group.pair.robot) ? pairing.getRobotNiceName(group.pair.robot) : group.pair.robot.replace(/[()]/g, "")) + " ";
-				var rmBtn = document.createElement("span");
-				rmBtn.textContent = "\u2716";	// heavy multiplication mark
-				rmBtn.style.cursor = "pointer";
-				rmBtn.addEventListener("click", function (ev) {
-					ev.stopPropagation();
-					pairing.endSession(group.pair.session_id);
-				}, false);
-				td.appendChild(rmBtn);
 				td.addEventListener("click", select);
 
-				var robot = pairing.getRobot(group.pair.robot);
+				// drop student or robot name
+				tr.addEventListener("dragenter", checkDragType);
+				tr.addEventListener("dragover", checkDragType);
+				tr.addEventListener("drop", function (ev) {
+					ev.stopPropagation();
+					ev.preventDefault();
+					var data = ev.dataTransfer.getData("text/plain");
+					if (data && /^[SG]\//.test(data)) {
+						ev.stopPropagation();
+						var studentName = data.slice(2);
+						pairing.addStudentToGroup(studentName, group.group_id, true);
+					} else if (data && /^R\//.test(data)) {
+						ev.stopPropagation();
+						dataParts = data.split("/");
+						var robotName = dataParts[1];
+						var robotNiceName = dataParts[2];
+						pairing.beginSession(robotName, group.group_id);
+					}
+				});
+
+				tr.appendChild(td);
+
+				// robot
 				td = document.createElement("td");
 				tr.appendChild(td);
-				if (robot && robot.hasFlash()) {
-					var btn = document.createElement("button");
-					btn.textContent = "flash";
-					btn.disabled = !robot.canFlash();
-					btn.addEventListener("mousedown", function () {
-						pairing.noRedraw = true;
-						robot.flash(true);
-						pairing.noRedraw = false;
+				if (group.pair) {
+					td.className = pairing.isGroupSelected(group.group_id) ? "rect selected" : "rect";
+					td.textContent = (/^\{/.test(group.pair.robot) ? pairing.getRobotNiceName(group.pair.robot) : group.pair.robot.replace(/[()]/g, "")) + " ";
+					var rmBtn = document.createElement("span");
+					rmBtn.textContent = "\u2716";	// heavy multiplication mark
+					rmBtn.style.cursor = "pointer";
+					rmBtn.addEventListener("click", function (ev) {
+						ev.stopPropagation();
+						pairing.endSession(group.pair.session_id);
 					}, false);
-					btn.addEventListener("mouseup", function () {
-						pairing.noRedraw = true;
-						robot.flash(false);
-						pairing.noRedraw = false;
-					}, false);
-					td.appendChild(btn);
+					td.appendChild(rmBtn);
+					td.addEventListener("click", select);
+
+					var robot = pairing.getRobot(group.pair.robot);
+					td = document.createElement("td");
+					tr.appendChild(td);
+					if (robot && robot.hasFlash()) {
+						var btn = document.createElement("button");
+						btn.textContent = "flash";
+						btn.disabled = !robot.canFlash();
+						btn.addEventListener("mousedown", function () {
+							pairing.noRedraw = true;
+							robot.flash(true);
+							pairing.noRedraw = false;
+						}, false);
+						btn.addEventListener("mouseup", function () {
+							pairing.noRedraw = true;
+							robot.flash(false);
+							pairing.noRedraw = false;
+						}, false);
+						td.appendChild(btn);
+					}
+				} else {
+					tr.appendChild(document.createElement("td"));
+				}
+
+				// button "remove"
+				td = document.createElement("td");
+				var btn = document.createElement("button");
+				btn.textContent = VPLTeacherTools.translate("remove");
+				btn.addEventListener("click", function () {
+					pairing.removeGroup(group.group_id);
+				}, false);
+				td.appendChild(btn);
+				tr.appendChild(td);
+
+				table.appendChild(tr);
+			}
+		});
+
+		// row to create new group
+		var tr = document.createElement("tr");
+		var td = document.createElement("td");
+		td.className = "rect";
+		td.textContent = "\u00a0";
+
+		// drop student name
+		tr.addEventListener("dragenter", checkDragType);
+		tr.addEventListener("dragover", checkDragType);
+		tr.addEventListener("drop", function (ev) {
+			ev.stopPropagation();
+			ev.preventDefault();
+			var data = ev.dataTransfer.getData("text/plain");
+			if (data && /^[SG]\//.test(data)) {
+				var studentName = data.slice(2);
+				pairing.addGroup(studentName);
+			}
+		}, false);
+
+		tr.appendChild(td);
+		table.appendChild(tr);
+
+		// drop student name outside a group -> delete
+		var body = document.body;
+		body.addEventListener("dragenter", checkDragType);
+		body.addEventListener("dragover", checkDragType);
+		body.addEventListener("drop", function (ev) {
+			ev.stopPropagation();
+			ev.preventDefault();
+			var studentName = ev.dataTransfer.getData("text/plain");
+			if (studentName.slice(0, 2) === "G/") {
+				studentName = studentName.slice(2);
+				var groupId = pairing.groupForStudent(studentName);
+				if (groupId) {
+					pairing.removeStudentFromGroup(studentName, groupId, true);
+					pairing.selectGroup(groupId);
+				}
+			}
+		}, false);
+
+		clearChildren("info");
+		var selectedGroup = pairing.getSelectedGroup();
+		if (selectedGroup && selectedGroup.robot) {
+			var div = document.createElement("div");
+			var p = document.createElement("p");
+			var groupDescr = selectedGroup.students.length > 0
+				? selectedGroup.students.join(", ")
+				: selectedGroup.group_id;
+			p.textContent = VPLTeacherTools.translate("Group") + ": " + groupDescr;
+			div.appendChild(p);
+			p = document.createElement("p");
+			p.style.overflowWrap = "break-word";
+			var a = document.createElement("a");
+			a.setAttribute("class", "url");
+			a.setAttribute("target", "_blank");
+			a.setAttribute("rel", "noopener");
+			p.appendChild(a);
+			div.appendChild(p);
+			var toolURL = VPLTeacherTools.makeVPLURL(selectedGroup, "$BRIDGE");
+			var url = document.location.origin + toolURL;
+
+			if (window.QRCode) {
+				var qrdiv = document.createElement("div");
+				div.appendChild(qrdiv);
+				var size = qrCodeSize();
+				var qrcode = new window.QRCode(qrdiv,
+					{
+						text: url,
+						width: size,
+						height: size,
+						correctLevel: QRCode.CorrectLevel.L
+					});
+				if ($SHORTENURL) {
+					pairing.shortenURL(url, function (shortenedURL) {
+						a.textContent = shortenedURL;
+						a.setAttribute("href", shortenedURL);
+						while (qrdiv.firstElementChild) {
+							qrdiv.removeChild(qrdiv.firstElementChild);
+						}
+						qrcode = new window.QRCode(qrdiv,
+							{
+								text: shortenedURL,
+								width: size,
+								height: size,
+								correctLevel: QRCode.CorrectLevel.L
+							});
+					});
+				} else {
+					a.textContent = url;
+					a.setAttribute("href", url);
 				}
 			} else {
-				tr.appendChild(document.createElement("td"));
+				if ($SHORTENURL) {
+					pairing.shortenURL(url, function (shortenedURL) {
+						a.textContent = shortenedURL;
+					});
+				} else {
+					a.textContent = url;
+					a.setAttribute("href", url);
+				}
 			}
-
-			// button "remove"
-			td = document.createElement("td");
-			var btn = document.createElement("button");
-			btn.textContent = VPLTeacherTools.translate("remove");
-			btn.addEventListener("click", function () {
-				pairing.removeGroup(group.group_id);
-			}, false);
-			td.appendChild(btn);
-			tr.appendChild(td);
-
-			table.appendChild(tr);
+			document.getElementById("info").appendChild(div);
 		}
-	});
-
-	// row to create new group
-	var tr = document.createElement("tr");
-	var td = document.createElement("td");
-	td.className = "rect";
-	td.textContent = "\u00a0";
-
-	// drop student name
-	tr.addEventListener("dragenter", checkDragType);
-	tr.addEventListener("dragover", checkDragType);
-	tr.addEventListener("drop", function (ev) {
-		ev.stopPropagation();
-		ev.preventDefault();
-		var data = ev.dataTransfer.getData("text/plain");
-		if (data && /^[SG]\//.test(data)) {
-			var studentName = data.slice(2);
-			pairing.addGroup(studentName);
-		}
-	}, false);
-
-	tr.appendChild(td);
-	table.appendChild(tr);
-
-	// drop student name outside a group -> delete
-	var body = document.body;
-	body.addEventListener("dragenter", checkDragType);
-	body.addEventListener("dragover", checkDragType);
-	body.addEventListener("drop", function (ev) {
-		ev.stopPropagation();
-		ev.preventDefault();
-		var studentName = ev.dataTransfer.getData("text/plain");
-		if (studentName.slice(0, 2) === "G/") {
-			studentName = studentName.slice(2);
-			var groupId = pairing.groupForStudent(studentName);
-			if (groupId) {
-				pairing.removeStudentFromGroup(studentName, groupId, true);
-				pairing.selectGroup(groupId);
-			}
-		}
-	}, false);
-
-	clearChildren("info");
-	var selectedGroup = pairing.getSelectedGroup();
-	if (selectedGroup && selectedGroup.robot) {
-		var div = document.createElement("div");
-		var p = document.createElement("p");
-		var groupDescr = selectedGroup.students.length > 0
-			? selectedGroup.students.join(", ")
-			: selectedGroup.group_id;
-		p.textContent = VPLTeacherTools.translate("Group") + ": " + groupDescr;
-		div.appendChild(p);
-		p = document.createElement("p");
-		p.style.overflowWrap = "break-word";
-		var a = document.createElement("a");
-		a.setAttribute("class", "url");
-		a.setAttribute("target", "_blank");
-		a.setAttribute("rel", "noopener");
-		p.appendChild(a);
-		div.appendChild(p);
-		var toolURL = VPLTeacherTools.makeVPLURL(selectedGroup, "$BRIDGE");
-		var url = document.location.origin + toolURL;
-
-		if (window.QRCode) {
-			var qrdiv = document.createElement("div");
-			div.appendChild(qrdiv);
-			var size = qrCodeSize();
-			var qrcode = new window.QRCode(qrdiv,
-				{
-					text: url,
-					width: size,
-					height: size,
-					correctLevel: QRCode.CorrectLevel.L
-				});
-            if ($SHORTENURL) {
-    			pairing.shortenURL(url, function (shortenedURL) {
-    				a.textContent = shortenedURL;
-    				a.setAttribute("href", shortenedURL);
-    				while (qrdiv.firstElementChild) {
-    					qrdiv.removeChild(qrdiv.firstElementChild);
-    				}
-    				qrcode = new window.QRCode(qrdiv,
-    					{
-    						text: shortenedURL,
-    						width: size,
-    						height: size,
-    						correctLevel: QRCode.CorrectLevel.L
-    					});
-    			});
-            } else {
-				a.textContent = url;
-				a.setAttribute("href", url);
-			}
-		} else {
-            if ($SHORTENURL) {
-    			pairing.shortenURL(url, function (shortenedURL) {
-    				a.textContent = shortenedURL;
-    			});
-            } else {
-				a.textContent = url;
-				a.setAttribute("href", url);
-			}
-		}
-		document.getElementById("info").appendChild(div);
 	}
 }
 
@@ -408,7 +413,7 @@ window.addEventListener("load", function () {
 		tdmURL: useTDM ? tdmURL : null,
 		jwsURL: useJWS ? jwsURL : null,
 		onRobots: function (robotArray, pairing) {
-    		fillRobotTable(robotArray, pairing);
+			fillRobotTable(robotArray, pairing);
 		},
 		robotLaunchURL: function (group) {
 			return url0 +
@@ -442,7 +447,7 @@ window.addEventListener("load", function () {
 			"!thymio": VPLTeacherTools.translate("(pupil local bridge)")
 		},
 		onStudents: function (studentArray) {
-    		fillStudentTable(studentArray, pairing);
+			fillStudentTable(studentArray, pairing);
 		},
 		onClasses: function (classes, currentClass) {
 			// fill class filter
